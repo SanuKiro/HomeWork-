4a57f10d9acf5793b7bb1787dbae192d5521b1d9
SmaEyeamZi <smaeyedreamee@outlook.com>
2021-11-12 16:47:10 +0800

    Progress 50%

--- Homework! .R
+++ Homework! .R
@@ -33,7 +33,18 @@ ggplot(data = M0G, aes(x = M0, y = GDP)) +
                    axis.title=element_text(size=25,face="bold"))+
   geom_smooth(method = 'lm')
 
+model<-lm(GDP~log(M0),data = M0G)
+ggplot(data = M0G, aes(x = M0, y = log(GDP))) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
 
+
+#model<-lm(M0~GDP,data = M0G)
+#ggplot(data = M0G, aes(x = GDP, y = M0)) + 
+#  geom_point()+theme(axis.text=element_text(size=13),
+#                     axis.title=element_text(size=25,face="bold"))+
+#  geom_smooth(method = 'lm')
 
 
 
@@ -51,14 +62,29 @@ M1G<-spread(M1G,key=type,value = value,1)
 ggplot(data = M1G, aes(x = M1, y = GDP)) + 
   geom_point()+theme(axis.text=element_text(size=13),
                      axis.title=element_text(size=25,face="bold"))
-model<-lm(GDP~M1,data = M1G)
+model1<-lm(GDP~M1,data = M1G)
 ggplot(data = M1G, aes(x = M1, y = GDP)) + 
   geom_point()+theme(axis.text=element_text(size=13),
                      axis.title=element_text(size=25,face="bold"))+
   geom_smooth(method = 'lm')
 
+model2<-lm(M1~GDP,data = M1G)
+ggplot(data = M1G, aes(x = GDP, y = M1)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
 
+model3<-lm(log(GDP)~M1,data = M1G)
+ggplot(data = M1G, aes(x = M1, y = log(GDP))) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
 
+model4<-lm(GDP~log(M1),data = M1G)
+ggplot(data = M1G, aes(x = log(M1), y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
 
 
 
@@ -73,12 +99,49 @@ ggplot(M2G, aes(x = years, y = value, group = type,linetype = type)) +
         legend.key.size = unit(20,"pt"))
 
 M2G<-spread(M2G,key=type,value = value,1)
-ggplot(data = M2G, aes(x = GDP, y = M2)) + geom_point()
-model<-lm(M2~GDP,data = M2G)
-ggplot(data = M2G, aes(x = GDP, y = M2)) + geom_point()+geom_smooth(method = 'lm')
+ggplot(data = M2G, aes(x = M2, y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))
+model1<-lm(GDP~M2,data = M2G)
+ggplot(data = M2G, aes(x = M2, y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
+
+model2<-lm(M2~GDP,data = M2G)
+ggplot(data = M2G, aes(x = GDP, y = M2)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
+
+model3<-lm(log(GDP)~M2,data = M2G)
+ggplot(data = M2G, aes(x = M2, y = log(GDP))) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
+
+model4<-lm(GDP~log(M2),data = M2G)
+ggplot(data = M2G, aes(x = log(M2), y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
+
+
+### CPI,M2 and GDP 
+
+##求各自变化率后另外储存
 
 
 
+
+
+
+
+
+
+
+
+##附加
 ##data1<-spread(data1,key=Type,value = value,1) ## 列行变量转制，更改表格模式
 ##ggplot(data1, aes(x = years, y = M0, group = 1)) +
 ##  geom_line()
