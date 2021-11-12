a3d71df603278fcbb4c62e241c42769c5bc0ed31
SmaEyeamZi <smaeyedreamee@outlook.com>
2021-11-12 09:46:34 +0800

    11.12 09:46 Progress 01

---
+++ Homework! .R
@@ -0,0 +1,79 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(readr)
+Data <- read_csv("~/Documents/R Project/For Project/Homework-/Data.csv")
+data1<-gather(Data,years,value,-1)
+data1$years<-factor(data1$years)
+ggplot(data1, aes(x = years, y = value, group = type,linetype = type)) +
+  geom_line(key_glyph = "timeseries") + geom_point() + theme(axis.text=element_text(size=13),
+  axis.title=element_text(size=25,face="bold"),
+  legend.text = element_text(size=20),
+  legend.title = element_text(size=23),
+  legend.key.size = unit(20,"pt"))
+  
+
+##GDP与M0
+M0G<-data.frame(data1[which(data1$type=="M0"|
+                              data1$type=="GDP"),])
+ggplot(M0G, aes(x = years, y = value, group = type,linetype = type)) +
+  geom_line(key_glyph = "timeseries") + geom_point() + 
+  theme(axis.text=element_text(size=13),axis.title=element_text(size=25,face="bold"),
+   legend.text = element_text(size=20),
+   legend.title = element_text(size=23),
+   legend.key.size = unit(20,"pt"))
+
+M0G<-spread(M0G,key=type,value = value,1)
+ggplot(data = M0G, aes(x = M0, y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                     axis.title=element_text(size=25,face="bold"))
+model<-lm(GDP~M0,data = M0G)
+ggplot(data = M0G, aes(x = M0, y = GDP)) + 
+  geom_point()+theme(axis.text=element_text(size=13),
+                   axis.title=element_text(size=25,face="bold"))+
+  geom_smooth(method = 'lm')
+
+
+
+
+
+##GDP与M1
+M1G<-data.frame(data1[which(data1$type=="M1"|
+                              data1$type=="GDP"),])
+ggplot(M1G, aes(x = years, y = value, group = type,linetype = type)) +
+  geom_line(key_glyph = "timeseries") + geom_point() + 
+  theme(axis.text=element_text(size=13),axis.title=element_text(size=25,face="bold"),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=23),
+        legend.key.size = unit(20,"pt"))
+
+M1G<-spread(M1G,key=type,value = value,1)
+ggplot(data = M1G, aes(x = GDP, y = M1)) + geom_point()
+model<-lm(M1~GDP,data = M1G)
+ggplot(data = M1G, aes(x = GDP, y = M1)) + geom_point()+geom_smooth(method = 'lm')
+
+
+
+
+
+
+##GDP与M2
+M2G<-data.frame(data1[which(data1$type=="M2"|
+                              data1$type=="GDP"),])
+ggplot(M2G, aes(x = years, y = value, group = type,linetype = type)) +
+  geom_line(key_glyph = "timeseries") + geom_point() + 
+  theme(axis.text=element_text(size=13),axis.title=element_text(size=25,face="bold"),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=23),
+        legend.key.size = unit(20,"pt"))
+
+M2G<-spread(M2G,key=type,value = value,1)
+ggplot(data = M2G, aes(x = GDP, y = M2)) + geom_point()
+model<-lm(M2~GDP,data = M2G)
+ggplot(data = M2G, aes(x = GDP, y = M2)) + geom_point()+geom_smooth(method = 'lm')
+
+
+
+##data1<-spread(data1,key=Type,value = value,1) ## 列行变量转制，更改表格模式
+##ggplot(data1, aes(x = years, y = M0, group = 1)) +
+##  geom_line()
